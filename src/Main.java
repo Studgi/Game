9b977d19996a96a8afedd0c6b7997ca954991876
Stodgy <80104967+Studgi@users.noreply.github.com>
2024-02-26 17:05:05 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int x=5;
+        int x=7;
         System.out.println(x);
     }
 }
