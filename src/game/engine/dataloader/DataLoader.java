8925d53401923fd0c2b30585872a557920a9c9e0
Stodgy <80104967+Studgi@users.noreply.github.com>
2024-03-03 21:09:02 +0200

    supported 2 argument weapon registry constructor & fix readline skipping

--- src/game/engine/dataloader/DataLoader.java
+++ src/game/engine/dataloader/DataLoader.java
@@ -10,23 +10,22 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class DataLoader {
-    static File titans = new File("." + File.pathSeparator + "titans.csv");
-    static File weapons = new File("." + File.pathSeparator + "weapons.csv");
-    private static final String TITANS_FILE_NAME = titans.toString();
-    private static final String WEAPONS_FILE_NAME = weapons.toString();
-
+    private static final File TITANS_FILE_NAME = new File("." + File.pathSeparator + "titans.csv");
+    private static final File WEAPONS_FILE_NAME = new File("." + File.pathSeparator + "weapons.csv");
 
     public static HashMap<Integer, TitanRegistry> readTitanRegistry() throws IOException {
         HashMap<Integer,TitanRegistry> TitansMap = new HashMap<>();
         BufferedReader br = new BufferedReader(new FileReader(TITANS_FILE_NAME));
-        while(br.readLine() != null){
-            String[] TitansStr = (br.readLine()).split(",");
+        String line = br.readLine();
+        while(line != null){
+            String[] TitansStr = line.split(",");
             int[] TitansInt = new int[TitansStr.length];
             for(int i = 0; i<TitansStr.length;i++){
                 TitansInt[i] = Integer.parseInt(TitansStr[i]);
             }
             TitanRegistry titanRegistry = new TitanRegistry(TitansInt[0],TitansInt[1],TitansInt[2],TitansInt[3],TitansInt[4],TitansInt[5],TitansInt[6]);
             TitansMap.put(TitansInt[0],titanRegistry);
+            line = br.readLine();
         }
         return TitansMap;
     }
@@ -34,8 +33,9 @@ public class DataLoader {
     public static HashMap<Integer, WeaponRegistry> readWeaponRegistry() throws IOException {
         HashMap<Integer,WeaponRegistry> WeaponsMap = new HashMap<>();
         BufferedReader br = new BufferedReader(new FileReader(WEAPONS_FILE_NAME));
-        while(br.readLine() != null){
-            String[] WeaponsStr = (br.readLine()).split(",");
+        String line = br.readLine();
+        while(line != null){
+            String[] WeaponsStr = line.split(",");
             int[] WeaponsInt = new int[WeaponsStr.length];
             for(int i = 0; i<WeaponsStr.length;i++){
                 if(i != 3)
@@ -44,11 +44,14 @@ public class DataLoader {
             WeaponRegistry weaponRegistry;
             if(WeaponsStr.length > 4){
                 weaponRegistry = new WeaponRegistry(WeaponsInt[0], WeaponsInt[1], WeaponsInt[2], WeaponsStr[3], WeaponsInt[4], WeaponsInt[5]);
-            } else {
+            } else if (WeaponsStr.length > 2) {
                 weaponRegistry = new WeaponRegistry(WeaponsInt[0], WeaponsInt[1], WeaponsInt[2], WeaponsStr[3]);
+            } else {
+                weaponRegistry = new WeaponRegistry(WeaponsInt[0], WeaponsInt[1]);
             }
 
             WeaponsMap.put(WeaponsInt[0],weaponRegistry);
+            line = br.readLine();
         }
         return WeaponsMap;
     }
